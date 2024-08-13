0d82a18c289d010e74093f7db3747a15ad61fc1f
Abrahan Piloto <abrahanpiloto@gmail.com>
2024-08-13 18:48:33 -0500

    funcion decimalToBinary simplificada

--- script.js
+++ script.js
@@ -21,27 +21,18 @@ numberInput.addEventListener("keydown", (e) => {
 );
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+ let binary = "";
+ 
 
-  if(input === 0) {
-    result.innerText = "0"
-    return
-  };
-  
-  while(input > 0) {
-    
-    const quotient = Math.floor(input/2);
-    const remainder = input % 2;
-    inputs.push(input)
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
-  };
-  result.innerText = remainders.reverse().join("");
-  console.log("inputs: ", inputs);
-  console.log("quotients: ", quotients);
-  console.log("remainders: ", remainders);
+ if(input === 0) {
+  binary = "0";
+ };
+
+ while (input > 0) {
+  binary = (input % 2) + binary;
+  input = Math.floor(input / 2 );
+   
+ }
+ result.innerText = binary;
 };
 
